28de8956b9954c55d79e41da3b9a92117c7ccc4f
mvmpvinicius <mvmpvinicius@gmail.com>
2020-06-17 16:27:30 +0100

    fix folder name

--- services/web_example_flask/config.py
+++ services/web_example_flask/config.py
@@ -11,5 +11,5 @@ UPLOAD_PATH = ''
 # Flask variables
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
-TEMPLATES_FOLDER = f"{os.getenv('APP_FOLDER')}/project/templates"
+STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/application/static"
+TEMPLATES_FOLDER = f"{os.getenv('APP_FOLDER')}/application/templates"
